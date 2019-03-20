f22eccee06402bcf7bcd87d19a08d9b02e019f17
Nicky Hoff <nick@hofff.com>
2019-03-20 15:57:49 +0100

    improve insert tags for stacked icons

--- src/InsertTags.php
+++ src/InsertTags.php
@@ -18,13 +18,15 @@ class InsertTags
 		if ($split[0] == 'icon-fa' && isset($split[1])) {
 			return '<i class="fa ' . $split[1] . '" aria-hidden="true"></i>';
 		} elseif ($split[0] == 'icon-fa-square-o' && isset($split[1])) {
-			return '<span class="fa-stack fa-lg"><i class="fa fa-square-o fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x" aria-hidden="true"></i></span>';
-		} elseif ($split[0] == 'icon-fa-circle' && isset($split[1])) {
-			return '<span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x fa-inverse" aria-hidden="true"></i></span>';
+			return '<span class="fa-stack"><i class="fa fa-square-o fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x" aria-hidden="true"></i></span>';
+		} elseif ($split[0] == 'icon-fa-circle-o' && isset($split[1])) {
+      return '<span class="fa-stack"><i class="fa fa-circle-o fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x" aria-hidden="true"></i></span>';
+    } elseif ($split[0] == 'icon-fa-circle' && isset($split[1])) {
+			return '<span class="fa-stack"><i class="fa fa-circle fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x fa-inverse" aria-hidden="true"></i></span>';
 		} elseif ($split[0] == 'icon-fa-square' && isset($split[1])) {
-			return '<span class="fa-stack fa-lg"><i class="fa fa-square fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x fa-inverse" aria-hidden="true"></i></span>';
+			return '<span class="fa-stack"><i class="fa fa-square fa-stack-2x" aria-hidden="true"></i><i class="fa ' . $split[1] . ' fa-stack-1x fa-inverse" aria-hidden="true"></i></span>';
 		} elseif ($split[0] == 'icon-fa-ban' && isset($split[1])) {
-			return '<span class="fa-stack fa-lg"><i class="fa ' . $split[1] . ' fa-stack-1x" aria-hidden="true"></i><i class="fa fa-ban fa-stack-2x text-danger" aria-hidden="true"></i></span>';
+			return '<span class="fa-stack"><i class="fa ' . $split[1] . ' fa-stack-1x" aria-hidden="true"></i><i class="fa fa-ban fa-stack-2x text-danger" aria-hidden="true"></i></span>';
 		} elseif ($split[0] == 'icon' && isset($split[1])) {
 			return '<i class="icon ' . $split[1] . '" aria-hidden="true"></i>';
 		}
